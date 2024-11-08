ef1c3ff8131879c52b0125414ddd6d9335560c58
NorGIt <jakob.kotera@gmail.com>
2024-11-08 11:00:31 +0100

    Update Main.java

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -33,8 +33,6 @@ class Calculator {
 
     }
 }
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Calculator calculator1 = new Calculator();
